cab96a72d39b07857cb12449424d8259caeb8038
Keanu <keanutimmermans8@gmail.com>
2021-02-14 19:07:39 +0100

    Wrote backend socket functionality.

--- src/index.js
+++ src/index.js
@@ -36,4 +36,58 @@ io.sockets.on('connection', (socket) => {
     array.push.apply(message, arguments);
     socket.emit('log', message);
   }
+
+  // What to do when we receive a message from the client?
+  socket.on('message', (message, room) => {
+    log(`Client said: ${message}`);
+    socket.in(room).emit('message', message, room);
+  });
+
+  // What to do when a room is created or joined?
+  socket.on('create or join', (room) => {
+    log(`Received request to create or join room ${room}`);
+
+    // Find clients in the current room:
+    var clientsInRoom = io.sockets.adapter.rooms[room];
+    var numClients = clientsInRoom
+      ? Object.keys(clientsInRoom.sockets).length
+      : 0;
+    log(`Room ${room} now has ${numClients} client(s)`);
+
+    // If no client is in the room, create a room and add the client.
+    if (numClients === 0) {
+      socket.join(room);
+      log(`Client ID ${socket.id} created room ${room}`);
+      socket.emit('created', room, socket.id);
+    } else if (numClients === 1) {
+      log(`Client ID ${socket.id} joined room ${room}`);
+      io.sockets.in(room).emit('join', room);
+      socket.join(room);
+      socket.emit('joined', room, socket.id);
+      io.sockets.in(room).emit('ready');
+    }
+
+    // If two clients are already present in the room, do not add this client.
+    // I am lazy, so there is a maximum of two clients.
+    else {
+      socket.emit('full', room);
+    }
+  });
+
+  // Utility event.
+  socket.on('ipaddr', () => {
+    var ifaces = os.networkInterfaces();
+    for (var dev in ifaces) {
+      ifaces[dev].forEach((details) => {
+        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+          socket.emit('ipaddr', details.address);
+        }
+      });
+    }
+  });
+
+  // Event for notifying other clients when a client leaves the room.
+  socket.on('bye', () => {
+    console.log('received bye');
+  });
 });
